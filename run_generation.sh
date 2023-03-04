ac446f9f1ba57fdf97a253bb94b419eea71944fe
lii <lii@stanford.edu>
2023-03-04 01:34:13 +0000

    added eval script

--- run_generation.sh
+++ run_generation.sh
@@ -1,4 +1,4 @@
 ##! /bin/bash
 
 # run_generation.py
-python run_generation.py --model_type ctrl --model_name_or_path ctrl
+python run_generation.py --model_type gpt2 --model_name_or_path /home/ubuntu/DeepLyrics/output/first_medium --length=50 --prompt="This is the lyrics of a love song by Drake: I just can't sleep tonight,"
